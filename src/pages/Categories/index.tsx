d824d1437d2088b151c2f40d30d8fa8e9478765f
gabrielluiz299 <gabrielluiz299@gmail.com>
2024-07-28 16:46:14 -0300

    projeto final ebac, esse projeto é a criação de site de delivery, itilizando styled components para estilizar, react router para navegar estre as paginas

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,51 @@
+import { useParams } from 'react-router-dom'
+
+import { useGetRestaurantQuery } from '../../services/api'
+
+import ProfileHeader from '../../components/ProfileHeader'
+import Banner from '../../components/Banner'
+import ListaPratos from '../../components/ListaDePratos'
+import Loader from '../../components/Loader'
+
+export type Prato = {
+  id: number
+  foto: string
+  preco: number
+  nome: string
+  descricao: string
+  porcao: string
+}
+
+export interface Restaurante {
+  id: number
+  titulo: string
+  destacado: boolean
+  tipo: string
+  avaliacao: number
+  descricao: string
+  capa: string
+  cardapio: Prato[]
+}
+
+type PratoParams = {
+  id: string
+}
+
+const Categories = () => {
+  const { id } = useParams() as PratoParams
+  const { data: restaurant } = useGetRestaurantQuery(id)
+
+  if (restaurant) {
+    return (
+      <>
+        <ProfileHeader />
+        <Banner />
+        <ListaPratos pratos={restaurant.cardapio} />
+      </>
+    )
+  }
+
+  return <Loader />
+}
+
+export default Categories
